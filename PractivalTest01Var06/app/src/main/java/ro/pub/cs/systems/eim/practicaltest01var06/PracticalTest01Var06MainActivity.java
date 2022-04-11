93c2229353f85399830c7ba68d004776f57f8447
Anita Grigore <anita.grigore1@gmail.com>
2022-04-11 10:30:05 +0300

    Add solution for task A

---
+++ PractivalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -0,0 +1,19 @@
+package ro.pub.cs.systems.eim.practicaltest01var06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PracticalTest01Var06MainActivity extends AppCompatActivity {
+    EditText editText1, editText2, editText3;
+    Button playButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var06_main);
+
+
+    }
+}
